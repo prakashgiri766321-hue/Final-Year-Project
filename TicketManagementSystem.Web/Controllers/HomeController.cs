b493e45426a948f3adc604762926dbe2182f5d5a
Prakash.Giri <prakash.giri766321@gmail.com>
2026-02-03 15:09:30 +0545

    Static page in index page for now.

--- TicketManagementSystem.Web/Controllers/HomeController.cs
+++ TicketManagementSystem.Web/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace TicketManagementSystem.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
