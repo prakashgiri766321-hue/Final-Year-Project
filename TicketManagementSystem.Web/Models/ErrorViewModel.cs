6aecceb44bcd3cf9b3eca1f2a49c3d7f4f1527a6
Prakash.Giri <prakash.giri766321@gmail.com>
2026-02-03 11:45:45 +0545

    Initial Commit for project

---
+++ TicketManagementSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TicketManagementSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
