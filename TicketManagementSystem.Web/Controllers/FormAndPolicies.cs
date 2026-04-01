7ab42ab5efad427d6cb545e6b5b02f3e747aadb2
Prakash.Giri <prakash.giri766321@gmail.com>
2026-04-01 22:20:03 +0545

    :art: all index pages are kept static for now

---
+++ TicketManagementSystem.Web/Controllers/FormAndPolicies.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketManagementSystem.Web.Controllers
+{
+    public class FormAndPolicies : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TicketManagementSystem.Web/Controllers/UserManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketManagementSystem.Web.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
