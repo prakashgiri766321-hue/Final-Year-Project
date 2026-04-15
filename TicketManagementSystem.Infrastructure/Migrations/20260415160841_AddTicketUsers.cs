55b920bebf8e51cffaba26b36d45d0b56618da5a
Prakash.Giri <prakash.giri766321@gmail.com>
2026-04-15 22:53:24 +0545

    :art: crud for ticket

---
+++ TicketManagementSystem.Infrastructure/Migrations/20260415160841_AddTicketUsers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicketUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedToId",
+                table: "Tickets",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById",
+                table: "Tickets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedToId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Tickets");
+        }
+    }
+}
--- TicketManagementSystem.Service/Interface/ITicketService.cs
+++ TicketManagementSystem.Service/Interface/ITicketService.cs
@@ -1,7 +1,10 @@
 ﻿
 using TicketManagementSystem.Application.DTOs;
+using TicketManagementSystem.Domain.Entities;
 
 public interface ITicketService
 {
     void CreateTicket(CreateTicketDto dto);
+    List<Ticket> GetAllTickets();
+    Ticket GetTicketById(int id);
 }
